307f74a4ba226808129ad8e0ec38601ddac01833
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-01 12:42:24 +0200

    created folder architecture for sass

--- kasa-immo/src/pages/Home/Home.jsx
+++ kasa-immo/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Home() {
-    <div>
-        <h1>Hello Home</h1>
-    </div>
+    return(
+        <div>
+            <h1>Hello Home</h1>
+        </div>
+    )
 }
 
 export default Home;
