2dab0f3ec521acf82825bfae0507d1df5841b9d4
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-01 15:03:29 +0200

    added color variables

--- kasa-immo/src/components/Header/Header.jsx
+++ kasa-immo/src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import '../../styles/components/_header.scss'
 
 function Header() {
     return (
-        <header>
-            <img className="navbar__logo" src="../../ assets / logo - kasa.png" alt="logo Kasa"></img>
-            <nav className="navbar">
+        <header className="header">
+            <img className="header__logo" src="../../ assets / logo - kasa.png" alt="logo Kasa"></img>
+            <nav className="header__navbar">
                 <Link className="navbar__navlink" to={'/'}>Home</Link>
                 <Link className="navbar__navlink" to={'/about'}>A propos</Link>
             </nav>
