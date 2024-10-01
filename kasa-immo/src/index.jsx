f3a316474eaf9cd504c2946e1d4d84474ef7b952
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-01 14:08:34 +0200

    feat: created router component

--- kasa-immo/src/index.jsx
+++ kasa-immo/src/index.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Error from './pages/Error/Error';
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header/Header';
+import AppRouter from './components/Router/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='*' element={<Error/>}></Route>
-      </Routes>
-    </Router>
+    <AppRouter>
+    </AppRouter>
   </React.StrictMode>
 );
 
