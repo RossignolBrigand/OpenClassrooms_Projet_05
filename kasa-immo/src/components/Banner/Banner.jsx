d27cdb278bd465a332fcd8e78d9544612ae45542
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-02 14:44:40 +0200

    Feat: updated banner elements

--- kasa-immo/src/components/Banner/Banner.jsx
+++ kasa-immo/src/components/Banner/Banner.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Banner() {
     return (
-        <div className="banner">
-            Banner
+        <div className="bannerImage">
+            <div className="bannerImage__content">
+                <h1 className="bannerImage__content--text">Chez vous, partout et ailleurs</h1>
+            </div>
         </div>
     )
 }
