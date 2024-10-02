086ca1245c05c7e858d9591472d5dfdbc50d8f07
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-02 16:24:45 +0200

    Feature: Created placeholder item page

---
+++ kasa-immo/src/components/CardContainer/CardContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CardContainer({ children }) {
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default CardContainer;
