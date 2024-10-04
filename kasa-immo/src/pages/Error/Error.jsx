f5cc59394b0bff6f6f9ceb7c7dd9225b7bf3e352
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-04 18:51:20 +0200

    Feature: Set up complete Error Page

--- kasa-immo/src/pages/Error/Error.jsx
+++ kasa-immo/src/pages/Error/Error.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import '../../styles/pages/_error.scss'
 
 function Error() {
-    return(
-    <div>
-        <h1>404: Page not found</h1>
-    </div>
+    return (
+        <div className="error">
+            <h1 className="error__code">404</h1>
+            <h2 className="error__content">Oups! La page que vous demandez n'existe pas.</h2>
+            <Link className="error__link" to={'/'}>Retourner vers la page d'accueil</Link>
+        </div>
     )
 }
 
