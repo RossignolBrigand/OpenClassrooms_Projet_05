1fee62c91719429dad502ecd09a40e255eb2fd29
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-28 17:24:21 +0100

    build v12

--- kasa-immo/src/index.jsx
+++ kasa-immo/src/index.jsx
@@ -24,7 +24,7 @@ import Error from './pages/Error/Error';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/OpenClassrooms_Projet_05'>
       <Layout>
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
