71930d79631e486612ee7a0dd9209f5493e10882
Thomas PINEAU <thomas.pineau93@proton.me>
2024-10-28 15:07:51 +0100

    deploy v7

--- kasa-immo/src/components/CardItem/CardItem.jsx
+++ kasa-immo/src/components/CardItem/CardItem.jsx
@@ -8,7 +8,7 @@ import './_cardItem.scss'
 function CardItem({ id, title, cover }) {
 
     return (
-        <Link to={`/Kasa/logements/${id}`} key={id} className='card'>
+        <Link to={`/logements/${id}`} key={id} className='card'>
             <div className='card-wrapper'>
                 <div className='card-wrapper__cover' style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.6) 0%, rgba(4, 4, 4, 0.6) 1%, rgba(10, 10, 10, 0.250) 41%, rgba(255, 255, 255, 0) 100%), url(${cover})` }} />
                 <h2 className='card-wrapper__content'>{title}</h2>
